4dd66ad7eb5d4881719cfae9830ff7d037006769
Talha Gergin <talhagergin6@gmail.com>
2025-01-04 00:30:05 +0300

    Initial Commit

---
+++ cs2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cs2
+//
+//  Created by Talha Gergin on 4.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cs2/cs2App.swift
@@ -0,0 +1,17 @@
+//
+//  cs2App.swift
+//  cs2
+//
+//  Created by Talha Gergin on 4.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct cs2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
